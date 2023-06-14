62569f81c3a36c68f820b480db9fbf632f7dd81d
Stefan Lemke <stban@posteo.de>
2023-06-14 11:30:29 +0200

    Inclusum backend boilerplate

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const connectDB = require("./dbinit");
+
+const PORT = process.env.PORT || 8080;
+
+connectDB();
+
+// middlewares
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Inclusum API");
+});
+
+app.listen(PORT, () => {
+  console.log(`server connected on ${PORT}`);
+});
