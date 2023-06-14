75879d17bb0a6c677090fa231c708c3e67cfa2e9
Stefan Lemke <stban@posteo.de>
2023-06-14 13:02:40 +0200

    implemented basic user login signup

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 const connectDB = require("./dbinit");
 
+const userRoutes = require("./routes/user");
+
 const PORT = process.env.PORT || 8080;
 
 connectDB();
@@ -13,6 +15,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Inclusum API");
